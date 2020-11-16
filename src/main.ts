d6e73e8b17122dae436ddd06ca1b8774e44785ae
ahwgs <ah_wgs@126.com>
2020-11-16 16:57:18 +0800

    feat: 新增一些web中间件

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import { ConfigService } from '@nestjs/config';
 import { EnvSwaggerOptions } from '@/config/env/swagger.config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { terminalHelpTextConsole } from '@/utils/terminal-help-text-console';
+import * as helmet from 'helmet';
+import * as compression from 'compression';
+import * as rateLimit from 'express-rate-limit';
 /**
  * 　启动函数
  */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: false,
     logger: false,
   });
 
@@ -17,8 +19,38 @@ async function bootstrap() {
   const swaggerOptions = configService.get<EnvSwaggerOptions>(
     'EnvSwaggerOptions',
   );
+
+  /**
+   * 设置CORS
+   * https://github.com/expressjs/cors#configuration-options
+   */
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
+  /**
+   * web安全
+   */
+  app.use(helmet());
+
+  /**
+   * 压缩中间件
+   */
+  app.use(compression());
+
   // 给请求添加prefix
   app.setGlobalPrefix(swaggerOptions.prefix);
+
+  /**
+   * 限速
+   */
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
 
   const options = new DocumentBuilder()
     .setTitle(swaggerOptions.title)
